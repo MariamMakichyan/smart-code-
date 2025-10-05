863ecbe74f925f0e288ff29acaa53e9f7a7dc8eb
Mariam Makichyan <mariam.a.makichyan@gmail.com>
2025-10-05 22:34:11 +0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+ import Nav from './components/Nav/Nav' 
+import './App.css'
+
+function App() {
+return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,7 @@
+function Nav() {
+    return(
+    <nav>  <div> <h1>hi
+        </h1></div></nav>
+    )
+}
+export default Nav
